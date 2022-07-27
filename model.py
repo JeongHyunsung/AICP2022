5c75efa891b7b41b322a8bda339f647b384a2dcf
JeongHyunsung <97275406+JeongHyunsung@users.noreply.github.com>
2022-07-27 22:07:16 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 class Text_Encoder(nn.Module):
     def __init__(self, batch_size, max_words, embedded_dim=512, hidden_dim=1024, num_layers=1, bidirectional=True):
 
+        super(Text_Encoder, self).__init__()
         self.batch_size = batch_size
         self.max_words = max_words
         self.embedded_dim = embedded_dim
@@ -18,22 +19,25 @@ class Text_Encoder(nn.Module):
         if self.bidirectional:
             self.D = 2
 
-        self.embedding = nn.Embedding(self.max_words, self.input_dim, padding_idx=0)
+        self.embedding = nn.Embedding(self.max_words, self.embedded_dim, padding_idx=0)
         self.LSTM = nn.LSTM(input_size=self.embedded_dim,
-                            hidden_Size=self.hidden_dim,
+                            hidden_size=self.hidden_dim,
                             num_layers=self.num_layers,
                             bidirectional=self.bidirectional,
                             batch_first=True)
 
-    def forward(self, x):  # input : (seq_length, batch_size, max_words)
+    def forward(self, x):  # input : (seq_length, batch_size), number range : 0~(max_words-1)
 
         h = torch.zeros((self.num_layers*self.D, self.batch_size, self.hidden_dim))
         c = torch.zeros((self.num_layers*self.D, self.batch_size, self.hidden_dim))
         torch.nn.init.xavier_normal_(h)
         torch.nn.init.xavier_normal_(c)
-
+        print(x.shape)
         x = self.embedding(x)  # (seq_length, batch_size, input_dim)
+
+        print(x.shape)
         x, (h, c) = self.LSTM(x, (h, c))  # (seq_length, batch_size, D*hidden_dim), ((D*num_layers, batch, hidden_dim), (D*num_layers, batch, hidden_dim))
 
+        h = torch.reshape(h, (self.num_layers, self.batch_size, self.D*self.hidden_dim))
         return h
 
