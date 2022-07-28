8325da415bf8ade903264f338b27ece5a3b7aa92
JeongHyunsung <97275406+JeongHyunsung@users.noreply.github.com>
2022-07-28 21:29:58 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 
 class Text_Encoder(nn.Module):
-    def __init__(self, batch_size, max_words, embedded_dim=512, hidden_dim=1024, num_layers=1, bidirectional=True):
+    def __init__(self, batch_size, max_words, embedded_dim=512, hidden_dim=1024, bidirectional=True, num_layers=1):
 
         super(Text_Encoder, self).__init__()
         self.batch_size = batch_size
@@ -20,11 +20,12 @@ class Text_Encoder(nn.Module):
             self.D = 2
 
         self.embedding = nn.Embedding(self.max_words, self.embedded_dim, padding_idx=0)
+        self.tanh = nn.Tanh()
         self.LSTM = nn.LSTM(input_size=self.embedded_dim,
                             hidden_size=self.hidden_dim,
                             num_layers=self.num_layers,
                             bidirectional=self.bidirectional,
-                            batch_first=True)
+                            batch_first=False)
 
     def forward(self, x):  # input : (seq_length, batch_size), number range : 0~(max_words-1)
 
@@ -32,12 +33,14 @@ class Text_Encoder(nn.Module):
         c = torch.zeros((self.num_layers*self.D, self.batch_size, self.hidden_dim))
         torch.nn.init.xavier_normal_(h)
         torch.nn.init.xavier_normal_(c)
-        print(x.shape)
-        x = self.embedding(x)  # (seq_length, batch_size, input_dim)
 
-        print(x.shape)
+        x = self.embedding(x)  # (seq_length, batch_size, input_dim)
+        x = self.tanh(x)
+
         x, (h, c) = self.LSTM(x, (h, c))  # (seq_length, batch_size, D*hidden_dim), ((D*num_layers, batch, hidden_dim), (D*num_layers, batch, hidden_dim))
 
         h = torch.reshape(h, (self.num_layers, self.batch_size, self.D*self.hidden_dim))
-        return h
+        if self.num_layers == 1:
+            h = torch.reshape(h, (self.batch_size, self.D*self.hidden_dim))
 
+        return h
