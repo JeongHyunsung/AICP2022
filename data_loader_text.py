f20b4e0ea184f453584f8f76444036235cdc0554
JeongHyunsung <97275406+JeongHyunsung@users.noreply.github.com>
2022-08-05 20:26:33 +0900

    Add files via upload

--- data_loader_text.py
+++ data_loader_text.py
@@ -1,14 +1,14 @@
-import json, pandas as pd
-import os
+import json, pandas as pd, numpy as np
+import os, random
 from torchtext.data import Field, BucketIterator
 
 
-class DataLoader_text:
-    def __init__(self, tokenizer, init_token, eos_token, unk_token):
-        self.tokenizer = tokenizer
-        self.init_token = init_token
-        self.eos_token = eos_token
-        self.unk_token = unk_token
+class data_loader_text:
+    def __init__(self):
+        self.dataset_frame = None
+        self.train_frame = None
+        self.valid_frame = None
+        self.eval_frame = None
         print("DataLoader_text initialized")
 
     def get_data(self, dataset_loc):
@@ -36,12 +36,27 @@ class DataLoader_text:
         dataset_frame = pd.DataFrame(dataset_2darr, columns=['history-10', 'history-9', 'history-8', 'history-7',
                                                              'history-6', 'history-5', 'history-4', 'history-3',
                                                              'history-2', 'history-1', 'current', 'next'])
-        return dataset_frame
+        self.dataset_frame = dataset_frame
+        return
+
+    def split_dataset(self, train_ratio=0.6, valid_ratio=0.2):
+        dataset_length = len(self.dataset_frame)
+        valid_split_idx = int(dataset_length*train_ratio)
+        eval_split_idx = int(dataset_length*(train_ratio+valid_ratio))
+        data_idx = list(range(dataset_length))
+        np.random.shuffle(data_idx)
+
+        train_idx, valid_idx, eval_idx = data_idx[:valid_split_idx], data_idx[valid_split_idx:eval_split_idx], data_idx[eval_split_idx:]
+        self.train_frame = self.dataset_frame.iloc[train_idx]
+        self.valid_frame = self.dataset_frame.iloc[valid_idx]
+        self.eval_frame = self.dataset_frame.iloc[valid_idx]
+        print(self.train_frame, self.valid_frame, self.eval_frame)
+        return
 
     def build_vocab(self):
         pass
 
-    def tokenization(self):
+    def tokenization(self, tokenizer, init_token, eos_token, unk_token):
         pass
 
     def token2index(self):
